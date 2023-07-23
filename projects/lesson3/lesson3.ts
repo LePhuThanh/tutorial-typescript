e2ffd484d88586445dfa765617fc3e5037144240
LePhuThanh <lpthanh710@gmail.com>
2023-07-23 18:51:02 +0700

    init project

---
+++ projects/lesson3/lesson3.ts
@@ -0,0 +1,9 @@
+let message: string = "Hello World";
+console.log(message);
+
+//move the mouse right located file: "Enter the first letter of the name project, after enter "tab"
+//1
+//tsc TS file => to run compiler TS = > JS
+//node JS file => to run JS
+//2
+//ts-node TS file => to run TS
---
+++ projects/lesson5/lesson5.ts
@@ -0,0 +1,6 @@
+const nameTS1 = 'Thanh';
+
+console.log('length = ', nameTS1.length)
+
+console.log('upPerCasse = ', nameTS1.toUpperCase())
+
